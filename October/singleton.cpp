50fdfef7ee20600dc416ee5399358f8bfe01f3d5
hrachm97 <hrachmkrtumyan@gmail.com>
2022-09-02 03:15:11 +0400

    Third variant of static failed. main outputs improved.

--- October/singleton.cpp
+++ October/singleton.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 
+//Variant 1
 class Singleton1 : public std::string {
     Singleton1(const char* str) : std::string(str) {
     }
@@ -18,22 +19,28 @@ public:
     using std::string::operator=;
 };
 
-//trying another approach
+//Variant 2
 struct : public std::string
 {   
     using std::string::operator=;
 } singleton2;
 
 int main() {
+    std::cout << "Singleton variant 1: Static member in a function" << std::endl;
     std::cout << Singleton1::get("Hello") << std::endl; // initializing
     std::cout << Singleton1::get("World") << std::endl; // initialization ignored
     std::cout << Singleton1::get() << std::endl;
     Singleton1::get() = "World"; // changing value
     std::string str(Singleton1::get()); // slicing
-    std::cout << str  << std::endl;
+    std::cout << str << std::endl << "******";
+    std::cout << std::endl << "Singleton variant 2: Unnamed class" << std::endl;
     singleton2 = "Hello World";
     std::cout << singleton2 << std::endl;
-    decltype(singleton2) singleton2copy; // second veriant of singleton disproved
-    singleton2copy = "Bye";
-    std::cout << singleton2copy << std::endl;
+    decltype(singleton2) second_singleton2;
+    std::cout << second_singleton2 << std::endl;
+    auto third_singleton2(singleton2);
+    std::cout << third_singleton2 << std::endl;
+    // second veriant of singleton disproved
+    // because it turned out to be recreatable with decltype or auto.
+    std::cout << "second veriant of singleton disproved" << std::endl;
 }
