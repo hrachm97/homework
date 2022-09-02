a645d68284d6a52525bdf3c836c8e946b20d78e6
hrachm97 <hrachmkrtumyan@gmail.com>
2022-09-03 03:31:15 +0400

    destructor, debugging, printing functions

--- October/forward_list.cpp
+++ October/forward_list.cpp
@@ -21,12 +21,22 @@ public:
             next->next = new forward_list(count - 2, value);
         }
     }
-    // forward_list(const forward_list& rhs) : value{ rhs.value }, next{ rhs.next ? new forward_list : nullptr } {
-    //     if(next)
-    //     {
-    //         next->value = rhs.next->value;
-    //     } 
-    // }
+    forward_list(const forward_list& rhs) : value{ rhs.value } {
+        if(rhs.next)
+        {
+            next = new forward_list(*(rhs.next));
+        } 
+    }
+    ~forward_list() 
+    {
+        if(next)
+        {
+            next->~forward_list();
+            std::cout << "Prepairing to free " << next;
+            delete next;
+            next = nullptr;
+        }
+    }
     size_t size() {
         if(next == nullptr) return 1;
         return 1 + next->size();
@@ -47,48 +57,48 @@ public:
         if(index == 1) return next->value;
         return (*next).get(index - 1).value;
     }
+    std::ostream& layout(std::ostream& os) const
+    {
+        os << "List: " << (void*)this << std::endl;
+        os << "Next: " << (void*)next << std::endl;
+        os << "Value: " << value << std::endl;
+        return os;
+    }
 };
 
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const forward_list<T>& obj) {
-    os << std::endl;
-    os << "List: " << &obj << std::endl;
-    os << "Next: " << obj.next << std::endl;
-    os << "Value: " << obj.value << std::endl;
+    os << obj.value;
+    if(obj.next) os << *(obj.next);
     return os;
 }
 
-// template <typename T>
-// std::ostream& operator<<(std::ostream& os, const forward_list<T>* obj) {
-//     size_t i = obj->m_size;
-//     while(i)
-//     {
-//         os << (*obj)(--i) << std::endl;
-//     }
-//     return os;
-// }
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const forward_list<T>* obj) {
+    obj->layout(os);
+    os << std::endl;
+    if(obj->next)
+    {
+        os << obj->next;
+    }
+    return os;
+}
 
 int main() {
     forward_list<int> a((size_t)4, 5);
-    std::cout << &a << std::endl;
-    std::cout << a.next << std::endl;
-    std::cout << a.next->next << std::endl;
-    std::cout << a.next->next->next << std::endl;
-    std::cout << a.next->next->next->next << std::endl;
-    std::cout << std::endl;
-    std::cout << a.get(0) << std::endl;
-    std::cout << a.get(1) << std::endl;
-    std::cout << a.get(2) << std::endl;
-    std::cout << a.get(3) << std::endl;
-    std::cout << a[0] << std::endl;
-    std::cout << a[1] << std::endl;
-    std::cout << a[2] << std::endl;
-    std::cout << a[3] << std::endl;
-    std::cout << a.size() << std::endl;
-    std::cout << a.end() << std::endl;
+    std::cout << "Printing a pointer of a forward_list prints all its members' layouts.\n"<< std::endl;
+    std::cout << &a;
+    std::cout << "List size: " << a.size() << std::endl << std::endl;
     a.push(7);
-    std::cout << a.get(4) << std::endl;
-    std::cout << a[4] << std::endl;
-    std::cout << a.size() << std::endl;
-    std::cout << a.end() << std::endl;
+    std::cout << "Printing a forward list directly, prints it's member's values: " << a << std::endl;
+    std::cout << std::endl;
+    auto b(a);
+    std::cout << &b;
+    std::cout << b << std::endl;
+    std::cout << std::endl;
+    std::cout << "Printing index [" << 4 << "]: " << b[4] << std::endl;
+    std::cout << std::endl;
+    std::cout << "Printing list member values since index [" << 2 << "]: " << b.get(2) << std::endl;
+    std::cout << std::endl;
+
 }
